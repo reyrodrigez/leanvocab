5ba745a58f64d7f737f611cd89ce22a5406f4b46
Laszlo Heves <laszlo.heves@gawker.com>
2015-07-25 01:30:27 +0200

    Word list view with angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,7 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+// setting global meta
+window.leanVocabMeta = {};
+
+// get enviroment switch
+window.leanVocabMeta.EVNI = (window.location.origin.indexOf('localhost') === -1) ? 'live' : 'dev';
+
+var leanVocabApp = angular.module('leanVocabApp', []);
