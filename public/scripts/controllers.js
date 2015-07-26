44639bc9199616b1b0c854e913cf0ec244f85f68
Laszlo Heves <laszlo.heves@gawker.com>
2015-07-27 01:19:00 +0200

    Make dev environment communicate with live API

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -3,9 +3,13 @@
 /* Controllers */
 
 leanVocabApp.controller('WordListCtrl', ['$scope', '$http', function($scope, $http) {
-  var url = (window.leanVocabMeta.EVNI === 'dev') ? 'http://localhost:2404/words' : '/words';
-  $http.get(url).success(function(data) {
-    $scope.words = data;
-  });
+  $http({
+          url: "//leanvocab.herokuapp.com/words",
+          method: "GET",
+          data: [],
+          params: {foo: $scope.foo}
+        }).success(function(data, status) {
+            $scope.words = data;
+        });
 
 }]);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,1 @@
-// setting global meta
-window.leanVocabMeta = {};
-
-// get enviroment switch
-window.leanVocabMeta.EVNI = (window.location.origin.indexOf('localhost') === -1) ? 'live' : 'dev';
-
 var leanVocabApp = angular.module('leanVocabApp', []);
